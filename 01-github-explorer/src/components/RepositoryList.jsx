f669a4883a04b06a7c77b55e20f649841adef5e7
Rafael R <ribeirorafaelmatheus@gmail.com>
2022-02-10 10:56:35 -0300

    useEffect and fecth github

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
@@ -10,6 +12,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/rafaelmgr12/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1> Lista de Repositórios</h1>
