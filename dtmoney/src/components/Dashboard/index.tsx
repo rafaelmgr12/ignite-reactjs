65cd4297a903c79e5fe9df1c26fbc16a5d887eb8
Rafael R <ribeirorafaelmatheus@gmail.com>
2022-02-20 15:55:56 -0300

    fixes

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { Summary } from '../Summary';
-import { TransactionTable } from '../TransactionTable';
+import { TransactionsTable } from '../TransactionTable';
 import { Container } from './styles'
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
-      <TransactionTable/>
+      <TransactionsTable/>
     </Container>
   );
 }
--- dtmoney/src/components/TransactionTable/index.tsx
+++ dtmoney/src/components/TransactionTable/index.tsx
@@ -8,17 +8,16 @@ interface Transaction {
   amount: number;
   type: string;
   category: string;
-  createdAt: Date;
+  createdAt: string;
 }
 
-export function TransactionTable() {
+export function TransactionsTable() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
-    api
-      .get("/transactions")
-      .then((response) => setTransactions(response.data.transactions));
-  });
+    api.get('transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, []);
 
   return (
     <Container>
@@ -31,21 +30,20 @@ export function TransactionTable() {
             <th>Data</th>
           </tr>
         </thead>
+
         <tbody>
-          {transactions.map((transaction) => (
-            <tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                {" "}
-                {new Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
                 }).format(transaction.amount)}
               </td>
               <td>{transaction.category}</td>
               <td>
-                {" "}
-                {new Intl.DateTimeFormat("pt-BR").format(
+                {new Intl.DateTimeFormat('pt-BR').format(
                   new Date(transaction.createdAt)
                 )}
               </td>
