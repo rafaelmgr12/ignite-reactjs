5715a77983c70c33555080a592a97d402304c130
Rafael R <ribeirorafaelmatheus@gmail.com>
2022-02-20 14:35:14 -0300

    saving data from form

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -13,7 +13,15 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+    console.log({ title, value, category, type });
+  }
 
   return (
     <Modal
@@ -30,10 +38,20 @@ export function NewTransactionModal({
           className="react-modal-close"
         />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
-        <input type="text" placeholder="Título" />
-        <input placeholder="Valor" type="Number" />
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          placeholder="Valor"
+          type="Number"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -41,8 +59,8 @@ export function NewTransactionModal({
             onClick={() => {
               setType("deposit");
             }}
-            isActive ={type === "deposit"}
-            activeColor = "green"
+            isActive={type === "deposit"}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -52,15 +70,19 @@ export function NewTransactionModal({
             onClick={() => {
               setType("withdraw");
             }}
-            isActive ={type === "withdraw"}
-            activeColor = "red"
+            isActive={type === "withdraw"}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createServer, Server } from "miragejs";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
+import { App } from './App';
 
 createServer({
   routes() {
-    this.namespace = "/api";
-    this.get("/transactions", () => {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
       return [
         {
           id: 1,
-          title: "Desenvolvimento de site",
-          amount: 12000,
-          type: "deposit",
-          category: "Food",
-          createAt: new Date(),
-        },
-      ];
-    });
-  },
-});
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
