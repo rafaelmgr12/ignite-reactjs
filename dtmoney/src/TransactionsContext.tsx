1067fcde5a7cd576abb358480b5066485d7bbbc2
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-02-22 17:54:44 -0300

    insertaion on table

--- dtmoney/src/TransactionsContext.tsx
+++ dtmoney/src/TransactionsContext.tsx
@@ -1,46 +1,52 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "./services/api";
 
-interface Transaction{
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
 }
 
-interface TransactionsProviderProps{
-    children: ReactNode
+interface TransactionsProviderProps {
+  children: ReactNode;
 }
 
-type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
 
 interface TransactionsContextData {
-    transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
-  }
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
 
 export const TransactionsContext = createContext<TransactionsContextData>(
-    {} as TransactionsContextData
+  {} as TransactionsContextData
 );
 
-
-export function TransactionsProvider({children}: TransactionsProviderProps) {
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
-    api.get("transactions").then(response => setTransactions(response.data.transactions));
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction:TransactionInput ) {
-      api.post("transactions", transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post("transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
-
   return (
-    <TransactionsContext.Provider value={{transactions,createTransaction}}>
-        {children}
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
     </TransactionsContext.Provider>
   );
 }
--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -15,19 +15,22 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-
-
-  const { createTransaction } = useContext(TransactionsContext)
-
+  const { createTransaction } = useContext(TransactionsContext);
 
   const [title, setTitle] = useState("");
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    createTransaction({ title, amount: value, category, type });
+    await createTransaction({ title, amount: value, category, type });
+
+    setTitle("");
+    setValue(0);
+    setCategory("");
+    setType("deposit");
+    onRequestClose();
   }
 
   return (
