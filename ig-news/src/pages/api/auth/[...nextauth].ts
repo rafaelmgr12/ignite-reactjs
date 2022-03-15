68f44d86d14acb9151db7285040eb0530f454912
Rafael R <ribeirorafaelmatheus@gmail.com>
2022-03-15 07:56:16 -0300

    Config faunadb

--- ig-news/src/pages/api/auth/[...nextauth].ts
+++ ig-news/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,9 +11,17 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user'
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
   ],
-})
+  callbacks: {
+    async signIn({user, account, profile}): Promise<boolean> {
+      const { email } = user;
+
+      await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      return true;
+    },
+  },
+});
