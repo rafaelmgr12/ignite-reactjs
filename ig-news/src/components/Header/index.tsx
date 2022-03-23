6f8d9cfe1d3a69cc8c96a6980cbbcc7c22b92cda
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-03-23 17:47:16 -0300

    add a new component

--- ig-news/src/components/Header/index.tsx
+++ ig-news/src/components/Header/index.tsx
@@ -1,5 +1,8 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
   return (
@@ -7,12 +10,14 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="http://localhost:3000/">
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts </a>
+          </ActiveLink>
         </nav>
-        <SignInButton/>
+        <SignInButton />
       </div>
     </header>
   );
