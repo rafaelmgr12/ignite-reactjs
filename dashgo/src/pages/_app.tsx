a7857597d25bf1af315bd903eec6ab64e3b907e6
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-06-03 06:03:47 -0300

    add user list

--- dashgo/src/pages/_app.tsx
+++ dashgo/src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../styles/theme'
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
-import { makeServer } from '../services/mirage'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -20,8 +21,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools/>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
