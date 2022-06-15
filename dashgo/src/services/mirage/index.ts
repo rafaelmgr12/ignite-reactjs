edb2a233b62a51c345d60a4de839878913328473
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-06-15 17:30:14 -0300

    add pagination components

--- dashgo/src/services/mirage/index.ts
+++ dashgo/src/services/mirage/index.ts
@@ -1,5 +1,5 @@
-import { createServer, Factory, Model } from 'miragejs'
-import faker from 'faker'
+import { createServer, Factory, Model, Response } from "miragejs";
+import faker from "faker";
 
 type User = {
   name: string;
@@ -10,13 +10,13 @@ type User = {
 export function makeServer() {
   const server = createServer({
     models: {
-      user: Model.extend<Partial<User>>({})
+      user: Model.extend<Partial<User>>({}),
     },
 
     factories: {
       user: Factory.extend({
         name(i: number) {
-          return `User ${i + 1}`
+          return `User ${i + 1}`;
         },
         email() {
           return faker.internet.email().toLowerCase();
@@ -24,24 +24,37 @@ export function makeServer() {
         createdAt() {
           return faker.date.recent(10);
         },
-      })
+      }),
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList("user", 200);
     },
 
     routes() {
-      this.namespace = 'api';
+      this.namespace = "api";
       this.timing = 750;
 
-      this.get('/users');
-      this.post('/users');
+      this.get("/users", function(schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
 
-      this.namespace = '';
-      this.passthrough()
-    }
-  })
+        const total = schema.all("user").length;
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
+      this.post("/users");
+
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
 
   return server;
 }
